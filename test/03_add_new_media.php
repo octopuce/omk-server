b2aa8606f79dd5e58533ad78f4bfed24e179a793
Benjamin Sonntag <benjamin@sonntag.fr>
2013-03-20 10:19:06 +0100

    starting to test add_new_media

---
+++ test/03_add_new_media.php
@@ -0,0 +1,19 @@
+<?php
+
+// TEST the API
+require_once("zz_config.php");
+
+/**
+ * Add a new media to the Transcoder. The given URL is absolute and static in that test-case
+ */
+$id=1;
+$result=call("app_new_media",
+	     array(
+		   "id" => $id,
+		   "url" => CLIENT_ROOT."zz_api.php?action=getvideo&key=".myhash($id)."id=".$id
+		   ));
+
+print_r($result);
+
+
+?>
