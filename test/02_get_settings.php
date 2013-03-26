03f9f9090d29cbb1df285b6b9a41ec074dc60dfc
Benjamin Sonntag <benjamin@sonntag.fr>
2013-03-26 22:05:34 +0100

    factorizing api call in test routines

--- test/02_get_settings.php
+++ test/02_get_settings.php
@@ -6,12 +6,7 @@ require_once("zz_config.php");
 /**
  * Returns the settings available in the transcoder.
  */
-$result=call("app_get_settings",
-	     array(
-		   "key" => API_KEY,
-		   "application" => APPLICATION_NAME,
-		   "version" => APPLICATION_VERSION,
-				      ));
+$result=call("app_get_settings");
 
 print_r($result);
 
--- test/03_add_new_media.php
+++ test/03_add_new_media.php
@@ -9,10 +9,6 @@ require_once("zz_config.php");
 $id=1;
 $result=call("app_new_media",
 	     array(
-		   "key" => API_KEY,
-		   "application" => APPLICATION_NAME,
-		   "version" => APPLICATION_VERSION,
-
 		   "id" => $id,
 		   "url" => CLIENT_ROOT."zz_api.php?action=getvideo&key=".myhash($id)."id=".$id
 		   ));
